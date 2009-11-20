7ee31f6b59ece31c05432afab38a5f31d594b8d1
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2009-11-20 07:45:08 +0000

    initial project structure

---
+++ trunk/org.osgilab.bundles/monitoradmin/src/main/java/org/osgilab/bundles/monitoradmin/Activator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2009 Dmytro Pishchukhin (http://osgilab.org)
+ *
+ * This file is part of OSGi Lab project (http://code.google.com/p/osgilab/).
+ *
+ * OSGi Lab modules are free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * OSGi Lab modules are distributed in the hope that they will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with OSGi Lab modules.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
+
+package org.osgilab.bundles.monitoradmin;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author dmytro.pishchukhin
+ */
+public class Activator implements BundleActivator {
+    public void start(BundleContext bundleContext) throws Exception {
+        // todo
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        // todo
+    }
+}
