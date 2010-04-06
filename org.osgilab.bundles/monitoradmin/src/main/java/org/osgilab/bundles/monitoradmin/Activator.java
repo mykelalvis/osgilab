a64f3a2ce88a0c802e45d2411dc60aa9d5b8c1d5
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-06 15:04:17 +0000

    update license

--- org.osgilab.bundles/monitoradmin/src/main/java/org/osgilab/bundles/monitoradmin/Activator.java
+++ org.osgilab.bundles/monitoradmin/src/main/java/org/osgilab/bundles/monitoradmin/Activator.java
@@ -1,20 +1,6 @@
 /*
- * Copyright (c) 2009 Dmytro Pishchukhin (http://osgilab.org)
- *
- * This file is part of OSGi Lab project (http://code.google.com/p/osgilab/).
- *
- * OSGi Lab modules are free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * OSGi Lab modules are distributed in the hope that they will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with OSGi Lab modules.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
  */
 
 package org.osgilab.bundles.monitoradmin;
