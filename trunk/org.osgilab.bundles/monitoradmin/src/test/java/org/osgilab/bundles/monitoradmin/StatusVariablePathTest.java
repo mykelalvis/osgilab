4cdb986dfd2d7f8ea5d2afe86dae25055076ec50
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-07 06:33:20 +0000

    add some tests

---
+++ trunk/org.osgilab.bundles/monitoradmin/src/test/java/org/osgilab/bundles/monitoradmin/StatusVariablePathTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.osgilab.bundles.monitoradmin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author dmytro.pishchukhin
+ */
+public class StatusVariablePathTest {
+    @Test
+    public void testParse() {
+        StatusVariablePath path = new StatusVariablePath("aaa/aaa");
+        Assert.assertEquals("aaa", path.getMonitorableId());
+        Assert.assertEquals("aaa", path.getStatusVariableId());
+
+        try {
+            new StatusVariablePath("aaa/aaa/aaa");
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            new StatusVariablePath("/aaa");
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+        }
+
+        try {
+            new StatusVariablePath("aaa.aaa./aaa");
+            Assert.fail();
+        } catch (IllegalArgumentException e) {
+        }
+    }
+}
