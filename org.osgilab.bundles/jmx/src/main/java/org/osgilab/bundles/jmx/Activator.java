3782739e3130973a9df64dfa718c1f113d05e874
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-11 17:15:50 +0000

    initial structure for jmx bundle

---
+++ org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/Activator.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.osgilab.bundles.jmx;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author dmytro.pishchukhin
+ */
+public class Activator implements BundleActivator {
+    public void start(BundleContext context) throws Exception {
+        // todo
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        // todo
+    }
+}
