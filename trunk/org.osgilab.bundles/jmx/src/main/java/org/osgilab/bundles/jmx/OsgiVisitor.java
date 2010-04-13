dac3108ceb604d96ef6e816b4fedf8e6469b0d16
dmytro.pishchukhin@gmail.com <dmytro.pishchukhin@gmail.com@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-13 12:43:58 +0000

    implement some methods

--- trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/OsgiVisitor.java
+++ trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/OsgiVisitor.java
@@ -1,7 +1,10 @@
 package org.osgilab.bundles.jmx;
 
+import org.osgi.framework.Bundle;
+
 /**
  * @author dpishchukhin
  */
 public interface OsgiVisitor {
+    Bundle getBundle(long id);
 }
--- trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/beans/BundleState.java
+++ trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/beans/BundleState.java
@@ -5,21 +5,28 @@
 
 package org.osgilab.bundles.jmx.beans;
 
+import org.osgi.framework.*;
 import org.osgi.jmx.framework.BundleStateMBean;
 import org.osgilab.bundles.jmx.OsgiVisitor;
 
 import javax.management.*;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.OpenDataException;
 import javax.management.openmbean.TabularData;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * BundleStateMBean Implementation
  *
  * @author dmytro.pishchukhin
  */
-public class BundleState extends AbstractMBean implements BundleStateMBean, NotificationBroadcaster {
+public class BundleState extends AbstractMBean implements BundleStateMBean, NotificationBroadcaster, BundleListener {
     private NotificationBroadcasterSupport nbs;
     private MBeanNotificationInfo[] notificationInfos;
+
+    private int sequenceNumber = 0;
 
     public BundleState(OsgiVisitor visitor) throws NotCompliantMBeanException {
         super(BundleStateMBean.class, visitor);
@@ -54,20 +61,52 @@ public class BundleState extends AbstractMBean implements BundleStateMBean, Noti
         return new String[0];  // todo
     }
 
-    public long getLastModified(long l) throws IOException {
-        return 0;  // todo
+    public long getLastModified(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        return bundle.getLastModified();
     }
 
-    public long[] getRegisteredServices(long l) throws IOException {
-        return new long[0];  // todo
+    public long[] getRegisteredServices(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        ServiceReference[] serviceReferences = bundle.getRegisteredServices();
+        long[] result;
+        if (serviceReferences == null) {
+            result = new long[0];
+        } else {
+            result = new long[serviceReferences.length];
+            for (int i = 0; i < serviceReferences.length; i++) {
+                result[i] = (Long) serviceReferences[i].getProperty(Constants.SERVICE_ID);
+            }
+        }
+        return result;
     }
 
     public long[] getRequiringBundles(long l) throws IOException {
         return new long[0];  // todo
     }
 
-    public long[] getServicesInUse(long l) throws IOException {
-        return new long[0];  // todo
+    public long[] getServicesInUse(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        ServiceReference[] serviceReferences = bundle.getServicesInUse();
+        long[] result;
+        if (serviceReferences == null) {
+            result = new long[0];
+        } else {
+            result = new long[serviceReferences.length];
+            for (int i = 0; i < serviceReferences.length; i++) {
+                result[i] = (Long) serviceReferences[i].getProperty(Constants.SERVICE_ID);
+            }
+        }
+        return result;
     }
 
     public int getStartLevel(long l) throws IOException {
@@ -78,8 +117,12 @@ public class BundleState extends AbstractMBean implements BundleStateMBean, Noti
         return null;  // todo
     }
 
-    public String getSymbolicName(long l) throws IOException {
-        return null;  // todo
+    public String getSymbolicName(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        return bundle.getSymbolicName();
     }
 
     public boolean isPersistentlyStarted(long l) throws IOException {
@@ -98,12 +141,20 @@ public class BundleState extends AbstractMBean implements BundleStateMBean, Noti
         return false;  // todo
     }
 
-    public String getLocation(long l) throws IOException {
-        return null;  // todo
+    public String getLocation(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        return bundle.getLocation();
     }
 
-    public String getVersion(long l) throws IOException {
-        return null;  // todo
+    public String getVersion(long id) throws IOException {
+        Bundle bundle = visitor.getBundle(id);
+        if (bundle == null) {
+            throw new IllegalArgumentException("Wrong Bundle ID: " + id);
+        }
+        return bundle.getVersion().toString();
     }
 
     public void addNotificationListener(NotificationListener listener, NotificationFilter filter, Object handback) throws IllegalArgumentException {
@@ -114,15 +165,32 @@ public class BundleState extends AbstractMBean implements BundleStateMBean, Noti
         nbs.removeNotificationListener(listener);
     }
 
-    public MBeanNotificationInfo[] getNotificationInfo() {
-        synchronized (this) {
-            if (notificationInfos == null) {
-                notificationInfos = new MBeanNotificationInfo[]{
-                        new MBeanNotificationInfo(new String[]{BundleStateMBean.EVENT},
-                                Notification.class.getName(), BundleStateMBean.EVENT)
-                };
-            }
-            return notificationInfos;
+    public synchronized MBeanNotificationInfo[] getNotificationInfo() {
+        if (notificationInfos == null) {
+            notificationInfos = new MBeanNotificationInfo[]{
+                    new MBeanNotificationInfo(new String[]{BundleStateMBean.EVENT},
+                            Notification.class.getName(), BundleStateMBean.EVENT)
+            };
+        }
+        return notificationInfos;
+    }
+
+    public synchronized void bundleChanged(BundleEvent event) {
+        Notification notification = new Notification(BundleStateMBean.EVENT, this, ++sequenceNumber,
+                System.currentTimeMillis());
+
+        try {
+            Bundle bundle = event.getBundle();
+            Map<String, Object> values = new HashMap<String, Object>();
+            values.put(IDENTIFIER, bundle.getBundleId());
+            values.put(LOCATION, bundle.getLocation());
+            values.put(SYMBOLIC_NAME, bundle.getSymbolicName());
+            values.put(EVENT, event.getType());
+            notification.setUserData(new CompositeDataSupport(BundleStateMBean.BUNDLE_EVENT_TYPE, values));
+
+            nbs.sendNotification(notification);
+        } catch (OpenDataException e) {
+           // todo: handle somehow
         }
     }
 }
