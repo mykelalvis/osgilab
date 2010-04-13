c62a2d6e51457651e876a74066a99cad34c92286
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-13 19:25:31 +0000

    some methods implementation

--- org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/Activator.java
+++ org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/Activator.java
@@ -5,9 +5,7 @@
 
 package org.osgilab.bundles.jmx;
 
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import org.osgi.framework.*;
 import org.osgi.jmx.framework.BundleStateMBean;
 import org.osgi.jmx.framework.FrameworkMBean;
 import org.osgi.jmx.framework.PackageStateMBean;
@@ -31,6 +29,7 @@ public class Activator implements BundleActivator, OsgiVisitor {
     private ObjectName packageStateMBeanObjectName;
     private BundleContext bc;
     private BundleState bundleState;
+    private ServiceState serviceState;
 
     public void start(BundleContext context) throws Exception {
         bc = context;
@@ -38,9 +37,11 @@ public class Activator implements BundleActivator, OsgiVisitor {
         registerJmxBeans();
 
         bc.addBundleListener(bundleState);
+        bc.addServiceListener(serviceState);
     }
 
     public void stop(BundleContext context) throws Exception {
+        bc.removeServiceListener(serviceState);
         bc.removeBundleListener(bundleState);
 
         unregisterJmxBeans();
@@ -65,7 +66,8 @@ public class Activator implements BundleActivator, OsgiVisitor {
         server.registerMBean(bundleState, bundleStateMBeanObjectName);
 
         serviceStateMBeanObjectName = new ObjectName(ServiceStateMBean.OBJECTNAME);
-        server.registerMBean(new ServiceState(this), serviceStateMBeanObjectName);
+        serviceState = new ServiceState(this);
+        server.registerMBean(serviceState, serviceStateMBeanObjectName);
 
         packageStateMBeanObjectName = new ObjectName(PackageStateMBean.OBJECTNAME);
         server.registerMBean(new PackageState(this), packageStateMBeanObjectName);
@@ -73,5 +75,40 @@ public class Activator implements BundleActivator, OsgiVisitor {
 
     public Bundle getBundle(long id) {
         return bc.getBundle(id);
+    }
+
+    public ServiceReference getServiceReferenceById(long id) {
+        try {
+            ServiceReference[] serviceReferences = bc.getAllServiceReferences(null, createServiceIdFilter(id));
+            if (serviceReferences != null && serviceReferences.length == 1) {
+                return serviceReferences[0];
+            }
+        } catch (InvalidSyntaxException e) {
+            e.printStackTrace();  // todo
+        }
+        return null;
+    }
+
+    public ServiceReference[] getAllServiceReferences() {
+        try {
+            ServiceReference[] serviceReferences = bc.getAllServiceReferences(null, null);
+            if (serviceReferences == null) {
+                return new ServiceReference[0];
+            }
+            return serviceReferences;
+        } catch (InvalidSyntaxException e) {
+            e.printStackTrace();  // todo
+        }
+        return new ServiceReference[0];
+    }
+
+    private String createServiceIdFilter(long id) {
+        StringBuilder builder = new StringBuilder();
+        builder.append('(');
+        builder.append(Constants.SERVICE_ID);
+        builder.append('=');
+        builder.append(id);
+        builder.append(')');
+        return builder.toString();
     }
 }
--- org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/OsgiVisitor.java
+++ org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/OsgiVisitor.java
@@ -1,10 +1,15 @@
 package org.osgilab.bundles.jmx;
 
 import org.osgi.framework.Bundle;
+import org.osgi.framework.ServiceReference;
 
 /**
  * @author dpishchukhin
  */
 public interface OsgiVisitor {
     Bundle getBundle(long id);
+
+    ServiceReference getServiceReferenceById(long id);
+
+    ServiceReference[] getAllServiceReferences();
 }
