88676f7d4b4d656325e930fecde8fb811ae968db
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-17 11:32:23 +0000

    - remove prefs out of bundle scope - define MonitorAdmin Bean methods

--- org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/service/monitor/MonitorAdmin.java
+++ org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/service/monitor/MonitorAdmin.java
@@ -8,6 +8,9 @@ package org.osgilab.bundles.jmx.service.monitor;
 import org.osgilab.bundles.jmx.beans.ServiceAbstractMBean;
 
 import javax.management.NotCompliantMBeanException;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.TabularData;
+import java.io.IOException;
 
 /**
  * @author dmytro.pishchukhin
@@ -17,5 +20,37 @@ public class MonitorAdmin extends ServiceAbstractMBean<org.osgi.service.monitor.
 
     public MonitorAdmin() throws NotCompliantMBeanException {
         super(MonitorAdminMBean.class);
+    }
+
+    public String getDescription(String path) throws IllegalArgumentException, IOException {
+        return null;  // todo
+    }
+
+    public CompositeData getStatusVariable(String path) throws IllegalArgumentException, IOException {
+        return null;  // todo
+    }
+
+    public String[] getMonitorableNames() throws IOException {
+        return new String[0];  // todo
+    }
+
+    public TabularData getStatusVariables(String monitorableId) throws IllegalArgumentException, IOException {
+        return null;  // todo
+    }
+
+    public String[] getStatusVariableNames(String monitorableId) throws IllegalArgumentException, IOException {
+        return new String[0];  // todo
+    }
+
+    public void switchEvents(String path, boolean on) throws IllegalArgumentException, IOException {
+        // todo
+    }
+
+    public boolean resetStatusVariable(String path) throws IllegalArgumentException, IOException {
+        return false;  // todo
+    }
+
+    public TabularData getRunningJobs() throws IOException {
+        return null;  // todo
     }
 }
