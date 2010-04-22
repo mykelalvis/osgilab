85eac9fe599b71460dbd2007c9d569057a964afe
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-22 18:51:51 +0000

    update sources header

--- trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/beans/LogVisitor.java
+++ trunk/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/beans/LogVisitor.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
 package org.osgilab.bundles.jmx.beans;
 
 /**
