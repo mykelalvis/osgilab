0177b136759ebd3bc21ca8dd128c05375584962a
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-23 21:01:18 +0000

    tag jmx 1.0.1

---
+++ tags/jmx-1.0.1/org.osgilab.bundles/jmx/src/main/java/org/osgilab/bundles/jmx/beans/AbstractMBean.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.osgilab.bundles.jmx.beans;
+
+import javax.management.NotCompliantMBeanException;
+import javax.management.StandardMBean;
+
+/**
+ * Abstract MBean
+ * 
+ * @author dpishchukhin
+ */
+public abstract class AbstractMBean extends StandardMBean {
+    protected OsgiVisitor visitor;
+    protected LogVisitor logVisitor;
+
+    protected AbstractMBean(Class<?> mbeanInterface) throws NotCompliantMBeanException {
+        super(mbeanInterface);
+    }
+
+    public void setVisitor(OsgiVisitor visitor) {
+        this.visitor = visitor;
+    }
+
+    public void setLogVisitor(LogVisitor logVisitor) {
+        this.logVisitor = logVisitor;
+    }
+
+    public void uninit() {
+        visitor = null;
+        logVisitor = null;
+    }
+}
