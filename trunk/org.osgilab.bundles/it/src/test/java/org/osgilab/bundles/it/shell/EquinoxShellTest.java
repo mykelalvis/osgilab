dd5a996e69e46956472e2ebcbaac2cf277b52bbc
dmytro.pishchukhin@gmail.com <dmytro.pishchukhin@gmail.com@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-04-23 13:50:33 +0000

    add shell integration tests

--- trunk/org.osgilab.bundles/it/src/test/java/org/osgilab/bundles/it/shell/EquinoxShellTest.java
+++ trunk/org.osgilab.bundles/it/src/test/java/org/osgilab/bundles/it/shell/EquinoxShellTest.java
@@ -5,7 +5,15 @@
 
 package org.osgilab.bundles.it.shell;
 
+import org.eclipse.osgi.framework.console.CommandProvider;
+import org.junit.Assert;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+import java.util.Collection;
+import java.util.Map;
 
 import static org.ops4j.pax.exam.CoreOptions.*;
 
@@ -15,6 +23,7 @@ import static org.ops4j.pax.exam.CoreOptions.*;
  * @author dpishchukhin
  */
 public class EquinoxShellTest extends AbstractShellTest {
+    @Configuration
     @Override
     public Option[] getShellSpecificConfiguration() {
         return options(
@@ -25,7 +34,45 @@ public class EquinoxShellTest extends AbstractShellTest {
     }
 
     @Override
-    public void testCommandsRegistration() {
-        //todo
+    public void testCommandsRegistration() throws Exception {
+        Map<String, String> defaultCommansList = getDefaultCommansList();
+
+        String groupName = "Custom Commands Group";
+        ServiceRegistration registration = registerShellService("group1", groupName, defaultCommansList);
+        ServiceReference[] commandReferences = bc.getServiceReferences("org.eclipse.osgi.framework.console.CommandProvider",
+                null);
+        Assert.assertNotNull(commandReferences);
+
+        int foundCommands = 0;
+        Collection<String> commandHelps = defaultCommansList.values();
+        for (ServiceReference commandReference : commandReferences) {
+            CommandProvider commandProvider = (CommandProvider) bc.getService(commandReference);
+            if (commandProvider.getHelp().indexOf(groupName) != -1) {
+                for (String commandHelp : commandHelps) {
+                    if (commandProvider.getHelp().indexOf(commandHelp) != -1) {
+                        foundCommands++;
+                    }
+                }
+            }
+        }
+        Assert.assertEquals(defaultCommansList.size(), foundCommands);
+
+        registration.unregister();
+
+        foundCommands = 0;
+        commandReferences = bc.getServiceReferences("org.eclipse.osgi.framework.console.CommandProvider", null);
+        if (commandReferences != null) {
+            for (ServiceReference commandReference : commandReferences) {
+                CommandProvider commandProvider = (CommandProvider) bc.getService(commandReference);
+                if (commandProvider.getHelp().indexOf(groupName) != -1) {
+                    for (String commandHelp : commandHelps) {
+                        if (commandProvider.getHelp().indexOf(commandHelp) != -1) {
+                            foundCommands++;
+                        }
+                    }
+                }
+            }
+        }
+        Assert.assertEquals(0, foundCommands);
     }
 }
--- trunk/org.osgilab.bundles/it/src/test/java/org/osgilab/bundles/it/shell/KnopflerfishShellTest.java
+++ trunk/org.osgilab.bundles/it/src/test/java/org/osgilab/bundles/it/shell/KnopflerfishShellTest.java
@@ -5,7 +5,15 @@
 
 package org.osgilab.bundles.it.shell;
 
+import org.junit.Assert;
+import org.knopflerfish.service.console.CommandGroup;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+
+import java.util.Collection;
+import java.util.Map;
 
 import static org.ops4j.pax.exam.CoreOptions.*;
 
@@ -15,20 +23,49 @@ import static org.ops4j.pax.exam.CoreOptions.*;
  * @author dpishchukhin
  */
 public class KnopflerfishShellTest extends AbstractShellTest {
+    @Configuration
     @Override
     public Option[] getShellSpecificConfiguration() {
         return options(
                 // test framework tyte
                 knopflerfish(),
                 provision(
-                        bundle("http://www.knopflerfish.org/snapshots/2.3.3.snapshot_trunk_2916/osgi/jars/console/console_all-2.0.1.jar"),
+                        bundle("http://www.knopflerfish.org/releases/2.3.3/osgi/jars/console/console_all-2.0.1.jar"),
                         mavenBundle().groupId("org.osgilab.bundles.shell").artifactId("knopflerfish").version("1.0.0")
                 )
         );
     }
 
     @Override
-    public void testCommandsRegistration() {
-        //todo
+    public void testCommandsRegistration() throws Exception {
+        Map<String, String> defaultCommansList = getDefaultCommansList();
+
+        String groupId = "group1";
+        String groupName = "Custom Commands Group";
+
+        ServiceRegistration registration = registerShellService(groupId, groupName, defaultCommansList);
+        ServiceReference[] commandReferences = bc.getServiceReferences("org.knopflerfish.service.console.CommandGroup",
+                "(" + CommandGroup.GROUP_NAME + "=" + groupId + ")");
+        Assert.assertNotNull(commandReferences);
+        Assert.assertEquals(1, commandReferences.length);
+
+        CommandGroup commandGroup = (CommandGroup) bc.getService(commandReferences[0]);
+        Assert.assertEquals(groupName, commandGroup.getShortHelp());
+        String longHelp = commandGroup.getLongHelp();
+        Collection<String> commandHelps = defaultCommansList.values();
+
+        int foundCommands = 0;
+        for (String commandHelp : commandHelps) {
+            if (longHelp.indexOf(commandHelp) != -1) {
+                foundCommands++;
+            }
+        }
+        Assert.assertEquals(2, foundCommands);
+
+        registration.unregister();
+
+        commandReferences = bc.getServiceReferences("org.knopflerfish.service.console.CommandGroup",
+                "(" + CommandGroup.GROUP_NAME + "=" + groupId + ")");
+        Assert.assertNull(commandReferences);
     }
 }
