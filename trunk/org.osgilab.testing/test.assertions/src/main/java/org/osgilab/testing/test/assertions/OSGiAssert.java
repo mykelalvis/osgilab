6ee5db1f2d3c8ae91193d47bacda29507f592448
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-05-31 19:41:58 +0000

    add some testing ideas

---
+++ trunk/org.osgilab.testing/test.assertions/src/main/java/org/osgilab/testing/test/assertions/OSGiAssert.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.osgilab.testing.test.assertions;
+
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author dmytro.pishchukhin
+ */
+public abstract class OSGiAssert {
+    protected static BundleContext bc;
+}
