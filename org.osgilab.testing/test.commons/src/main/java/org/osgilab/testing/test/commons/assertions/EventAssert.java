3a5e53fe0de76edb57b6c2491cd7a576647520f1
dmytro.pishchukhin <dmytro.pishchukhin@c4d1f7d0-bfa1-11de-a0e9-83d03c287164>
2010-06-01 05:40:50 +0000

    add some testing ideas

--- org.osgilab.testing/test.commons/src/main/java/org/osgilab/testing/test/commons/assertions/EventAssert.java
+++ org.osgilab.testing/test.commons/src/main/java/org/osgilab/testing/test/commons/assertions/EventAssert.java
@@ -3,12 +3,10 @@
  * This program is made available under the terms of the MIT License.
  */
 
-package org.osgilab.testing.test.assertions;
+package org.osgilab.testing.test.commons.assertions;
 
 /**
  * @author dmytro.pishchukhin
  */
-public class ServiceAssert extends OSGiAssert {
-    public static void assertServiceRegistered(String[] classes, String filter) {
-    }
+public class EventAssert {
 }
---
+++ org.osgilab.testing/test.commons/src/main/java/org/osgilab/testing/test/commons/assertions/ServiceAssert.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2010 Dmytro Pishchukhin (http://osgilab.org)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.osgilab.testing.test.commons.assertions;
+
+/**
+ * @author dmytro.pishchukhin
+ */
+public class ServiceAssert {
+    public static void assertServiceAvailable(String[] classes, String filter) {
+    }
+
+    public static void assertServiceUnavailable(String[] classes, String filter) {
+    }
+}
