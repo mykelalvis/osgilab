63a365f90fc1369a4079a91b6297d8314f223084
dmytro.pishchukhin <dmytro.pishchukhin@gmail.com>
2011-04-01 21:01:38 +0000

    add shell felix-gogo module

---
+++ org.knowhowlab.osgi/shell/felix-gogo/src/main/java/org/knowhowlab/osgi/shell/felixgogo/Activator.java
@@ -0,0 +1,19 @@
+package org.knowhowlab.osgi.shell.felixgogo;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Felix GoGo shell adapter activator
+ *
+ * @author dpishchukhin
+ */
+public class Activator implements BundleActivator {
+    public void start(BundleContext bundleContext) throws Exception {
+        //todo
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        //todo
+    }
+}
